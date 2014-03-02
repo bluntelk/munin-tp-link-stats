a85c0d0ddaaf48bcfd42aa68e95e57a522f92833
Jason Playne <jason@jasonplayne.com>
2014-03-02 11:53:39 +0800

    creating common functions and specific executables
    possible detect symlink'd names and switch based on that

---
+++ common/common.go
@@ -0,0 +1,102 @@
+package common
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type ResponseStruct interface {
+	GetAutoConfig() bool
+	GetConfig() []string
+	GetData() []string
+}
+
+type UpDownStats struct {
+	Up, Down, Min, Max float64
+	Unit               string
+}
+type ModemStats struct {
+	Snr     UpDownStats
+	Atten   UpDownStats
+	Sync    UpDownStats
+	MaxRate UpDownStats
+	Power   UpDownStats
+	Crc     UpDownStats
+}
+
+type ModemUrl struct {
+	Host, Path, Username, Password string
+}
+
+func (m *ModemUrl)AsUrl() string {
+	return fmt.Sprintf("http://%s:%s@%s%s", m.Username, m.Password, m.Host, m.Path)
+}
+
+func GetModemUrl() ModemUrl {
+	var m ModemUrl
+	m.Host = GetValueFromEnv("tplink_host", "")
+	m.Path = GetValueFromEnv("tplink_path", "/status/status_deviceinfo.htm")
+	m.Username = GetValueFromEnv("tplink_username", "admin")
+	m.Password = GetValueFromEnv("tplink_password", "")
+
+	PluginDebugPrint(fmt.Sprintf("Using URL %s",m.AsUrl()))
+	return m
+}
+
+func GetValueFromEnv(value, default_value string) string {
+	ret_value := os.Getenv(value)
+	if ret_value == "" {
+		ret_value = default_value
+	}
+	return ret_value
+}
+
+func ShowResponse(response string) {
+	fmt.Println(response)
+}
+
+func GetAction() string {
+	action := "data"
+	if len(os.Args) > 1 {
+		action = os.Args[1]
+	}
+
+	PluginDebugPrint(fmt.Sprintf("Command Line Action: %s", action))
+	return action
+}
+
+func FetchData(url ModemUrl) ModemStats {
+	var stats ModemStats
+
+	return stats;
+}
+
+func HandleAction(action string, handlers ResponseStruct) string {
+	ret := ""
+	if action == "autoconf" {
+		PluginDebugPrint("Performing Action: Auto Config")
+		if handlers.GetAutoConfig() {
+			ret = "yes"
+		} else {
+			ret = "no"
+		}
+
+	} else if action == "config" {
+		PluginDebugPrint("Performing Action: Config")
+		ret = strings.Join(handlers.GetConfig(), "\n");
+
+	} else {
+		PluginDebugPrint("Performing Action: Gather Data")
+		ret = strings.Join(handlers.GetData(), "\n");
+	}
+
+	return ret
+}
+
+func PluginDebugPrint(message string) {
+	if GetValueFromEnv("MUNIN_DEBUG", "") == "1" {
+		fmt.Println(fmt.Sprintf("#DEBUG: %s", message))
+	}
+
+}
--- main.go
+++ main.go
@@ -12,14 +12,46 @@ func handleAction(action string) string {
 		ret = "yes"
 	} else if action == "config" {
 		var config = []string{
+			"multigraph snr",
+
+			"multigraph atten",
+			"graph_args --base 1000 -l 0 --upper-limit 25000",
+			"graph_vlabel mbits",
+			"graph_title Internet Connection Sync Speed",
+
+
+			"multigraph sync",
+			"graph_args --base 1000 -l 0 --upper-limit 25000",
+			"graph_vlabel mbits",
+			"graph_title Internet Connection Sync Speed",
+
+			"multigraph sync_max",
+			"graph_args --base 1000 -l 0 --upper-limit 25000",
+			"graph_vlabel mbits",
+			"graph_title Max Sync speed",
+
+			"multigraph power",
+			"graph_args --base 1000 -l 0 --upper-limit 50",
+			"graph_vlabel dbm",
+			"graph_title Power",
+
+			"multigraph crc",
+			"graph_args --base 1000 -l 0",
+			"graph_vlabel dbm",
+			"graph_title Power",
+
+
+
+
 			"graph_args --base 1000 -l 0 --upper-limit 25000",
 			"graph_vlabel mbits",
 			"graph_title Internet Connection",
 			"graph_category network",
-			"graph_info Shows the historical Sync speed of the modem",
+			"graph_info Shows the historical connection health of the modem",
 			"graph_order snr, atten, sync, sync_max, power, crc, status",
 
 			"snr.label Signal to Noise Ratio",
+			"snr.info SNR below 6 is bad it means that there is too much noise for a good connection. You want a steady straight line with little variation.",
 
 			"atten.label Signal Attenuation",
 
