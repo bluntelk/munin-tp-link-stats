dea94cd1e9271151d3af173dc5d5a78e93b33d8f
Jason Playne <jason@jasonplayne.com>
2014-03-03 12:01:40 +0800

    values <= 0 are not reported as U

--- common/atten.go
+++ common/atten.go
@@ -37,9 +37,18 @@ func (h *AttenHandlers) GetData() []string {
 
 	modem_data := FetchData(GetModemUrl())
 
+	var upstream_value, downstream_value string
+
+	if upstream_value = fmt.Sprintf("%0.2f", modem_data.Atten.Up); upstream_value == "0.00" {
+		upstream_value = "U"
+	}
+	if downstream_value = fmt.Sprintf("%0.2f", modem_data.Atten.Down); downstream_value == "0.00" {
+		downstream_value = "U"
+	}
+
 	var data = []string{
-		fmt.Sprintf("upstream.value %0.2f", modem_data.Atten.Up),
-		fmt.Sprintf("downstream.value %0.2f", modem_data.Atten.Down),
+		fmt.Sprintf("upstream.value %s", upstream_value),
+		fmt.Sprintf("downstream.value %s", downstream_value),
 	}
 	return data
 }
